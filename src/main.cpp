85d44ba5bf32e1cb7a71cb90c608933596c2e1fc
josephyzy <josephyang@gmail.com>
2021-12-21 01:21:32 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 #include <Arduino.h>
 
 #include "TFT_eSPI.h"
-#include "Free_Fonts.h"   //include free fonts library from https://github.com/Seeed-Studio/Seeed_Arduino_LCD/blob/master/examples/320x240/Free_Font_Demo/Free_Fonts.h
+#include "Free_Fonts.h" //include free fonts library from https://github.com/Seeed-Studio/Seeed_Arduino_LCD/blob/master/examples/320x240/Free_Font_Demo/Free_Fonts.h
 
-#define FONT FMB12    // See "include/Free_Fonts.h" for options
+#define FONT FMB12 // See "include/Free_Fonts.h" for options
 
 TFT_eSPI tft;
-TFT_eSprite spr = TFT_eSprite(&tft);    //Initializing buffer
+TFT_eSprite spr = TFT_eSprite(&tft); //Initializing buffer
 
 void loadPage(String headerText, uint16_t headerTextCol, uint16_t headerBkgCol, uint16_t bkgCol);
 
@@ -17,7 +17,6 @@ void setup()
   tft.setRotation(2);
 
   spr.createSprite(TFT_WIDTH, TFT_HEIGHT); // Create buffer (portrait)
-
 }
 
 void loop()
@@ -25,7 +24,6 @@ void loop()
   // put your main code here, to run repeatedly:
 
   loadPage("Home", TFT_BLACK, TFT_WHITE, TFT_GREENYELLOW);
-
 }
 
 void loadPage(String headerText, uint16_t headerTextCol, uint16_t headerBkgCol, uint16_t bkgCol)
@@ -34,9 +32,19 @@ void loadPage(String headerText, uint16_t headerTextCol, uint16_t headerBkgCol, 
 
   spr.fillRect(0, 0, 240, 30, headerBkgCol); // Header portion
   spr.setFreeFont(FONT);
-//  spr.setTextSize(1);
+  //  spr.setTextSize(1);
   spr.setTextColor(headerTextCol);
   spr.drawString(headerText, 5, 6);
+
+  unsigned int rowHeight = 20;
+  unsigned int topMargin = 33;
+  unsigned int leftMargin = 0;
+  for (unsigned int i = 0; i < 13; i++)
+  {
+    spr.drawString("Ln " + String(i) + " posY = " + String(topMargin + i * rowHeight), leftMargin, topMargin + i * rowHeight);
+  }
+
+  spr.drawString("01234567890123456789", leftMargin, 293);
 
   spr.pushSprite(0, 0); //Push to LCD
 }
